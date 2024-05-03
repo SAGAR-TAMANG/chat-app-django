9a6247eb64378e7e51530c5cb46717dd272dd885
Sagar Tamang <sagar.bdr0000@gmail.com>
2024-05-03 15:56:24 +0530

    latest added consumers and routers

--- a_core/settings.py
+++ a_core/settings.py
@@ -19,6 +19,7 @@ ALLOWED_HOSTS = ['localhost', '127.0.0.1', '*']
 # Application definition
 
 INSTALLED_APPS = [
+    'daphne',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -70,7 +71,8 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'a_core.wsgi.application'
+# WSGI_APPLICATION = 'a_core.wsgi.application'
+ASGI_APPLICATION = 'a_core.asgi.application'
 
 
 # Database
